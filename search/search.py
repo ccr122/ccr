4dcd841b2bc6a219ed31900902e07c751a1c9e71
caspern1 <casperneo@uchicago.edu>
2016-02-29 22:22:47 +0000

    x

--- search/search.py
+++ search/search.py
@@ -8,6 +8,7 @@ import pandas as pd
 import csv
 import pickle
 import os.path
+import re
 
 '''
 exhibits = {ex_id: { word in decscription : number of occurance } }
@@ -19,43 +20,78 @@ Also if we have time we should be able to update these data
     without reconstructing everything
 
 '''
+
+##### Data related: getting and saving files, generating pandas tables
+
 FILE_PATHS = {  'dim'         : 'pickled_dim',
-                'exhibits'    : 'pickled_exhibits',
-                'comparison'  : 'pickled_comparison',
+#                'exhibits'    : 'pickled_exhibits',
+                'comparison'  : 'comparison_table',
                 'ex_desc_csv' : '../csvs/ex_id_to_ex_desc_parsed.csv'
                 }
 
-def make_dim_exhibits():
+def make_dim_exhibits(museums):
     '''
-    This function reads the parsed exhibits and returns (dim,exhibits) 
-    dim is a list of [all words]
-    exhibits is a dict that maps exhibit_id to a dict that maps word
-    to number of occurances in that exhibit's description/title
-    '''  
-    dim = []
-    exhibits = {}
-    with open(FILE_PATHS['ex_desc_csv']) as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in reader:
-            ex_id = row[0]
-            w = row[1]
-            if ex_id not in exhibits:
-                exhibits[ex_id] = {}
-            if w not in exhibits[ex_id]:
-                exhibits[ex_id][w] = 0
-            exhibits[ex_id][w] += 1
-            
-            if w not in dim:
-                dim.append(w)
-    with open(FILE_PATHS['dim'],'w') as f:
-        pickle.dump(dim,f)
-        f.close()
-    with open(FILE_PATHS['exhibits'],'w') as g:
-        pickle.dump(exhibits,g)
-        g.close()
+    takes a list of museum ids: museums = ['001','002',...]
+    makes dim - set of all words that appeared in dataframe
+    makes filtered - dataframe filtered by museum
+    '''
+    exhibits = pd.read_csv( FILE_PATHS['ex_desc_csv'] )
+    if museums is not None:
+        filter_ = [str(x) for x in exhibits['ex_id']]
+        museums = [m.lstrip('0') for m in museums]
+        filter_ = [ x[:-2] in museums for x in filter_]
+        exhibits = exhibits[filter_]
+    dim = set(exhibits.groupby('word').groups.keys())
+    return (dim, exhibits)
+
+def build_comparison_table():
+    '''
+    Makes table of cosine_distance between descriptions
+    index and columns are all exhibit ids
+    '''
+    (dim,exhibits) = make_dim_exhibits(None)
+    comparison = pd.DataFrame(  columns = exhibits.keys(),
+                                index = exhibits.keys())
+                                
+    ex_dict = exhibits.groupby('ex_id')
+    for x in ex_dict:
+        rows = ex_dict[x]
+        words = [exhibits.loc[ row,'word'] for row in rows]
+        ex_dict[x] = words
+    
+    for ex in ex_dict:
+        comparison[ex][ex]=0.0
+    for (ex1,ex2) in itertools.combinations(ex_dict,2):
+    
+        d = cosine_distance(ex_dict[ex1] ,ex_dict[ex2],dim)
+        comparison[ex1][ex2] = d
+        comparison[ex2][ex1] = d
+        
+    comparison.to_csv(FILE_PATHS['comparison'])
+    
+def update_comparison_table(exhibits,comparison):
+    '''
+    In case we decide to get more museums we should update comparison
+    rather than build it from scratch
+    '''
+    pass
+
+def get_comparison_table():
+    c = FILE_PATHS['comparison']
+    if os.path.isfile(c):
+        return pd.read_csv(c)
+    else:
+        build_comparison_table() #makes and saves table
+    assert os.path.isfile(c)
+    return pd.read_csv(c)
+
+######### Cosine similarity
 
 def make_vector(words,dim):
+    '''
+    Changes a dict of {word:word count}
+    into list (vector)
+    '''
     vect = []
     for d in dim:
         x = words.get(d)
@@ -75,56 +111,13 @@ def cosine_distance(words1,words2,dim):
     b = make_vector(words2,dim)
     return spatial.distance.cosine(a,b)
 
-def build_comparison_table():
-    '''
-    Makes table of cosine_distance between descriptions
-    index and columns are all exhibit ids
-    '''
-    exhibits = get_files('exhibits')
-    comparison = pd.DataFrame(  columns = exhibits.keys(),
-                                index = exhibits.keys())
-    
-    for ex in exhibits.keys():
-        comparison[ex][ex]=0.0
-    for (ex1,ex2) in itertools.combinations(exhibits.keys(),2):
-        d = cosine_distance(exhibits[ex1] ,exhibits[ex2],dim)
-        comparison[ex1][ex2] = d
-        comparison[ex2][ex1] = d
-        
-    with open(FILE_PATHS['comparison'],'w') as f:
-        pickle.dump(comparison,f)
-        f.close()
 
-def update_comparison_table(exhibits,comparison):
-    '''
-    In case we decide to get more museums we should update comparison
-    rather than build it from scratch
-    '''
-    pass
-
-def get_pickled_files(s):
-    path = FILE_PATHS[s]
-    if os.path.isfile( path ):
-        with open( path ,'r') as f:
-            return pickle.load(f)
-    else:
-        return None
-
-def get_files(s):
-    unpickled = get_pickled_files(s)
-    if unpickled is not None:
-        return unpickled
-    else:
-        FILE_MAKERS = {'dim' : make_dim_exhibits,
-                    'exhibits' : make_dim_exhibits,
-                    'comparison' : build_comparison_table}
-        FILE_MAKERS[s]()
-        unpickled = get_pickled_files(s)
-        assert unpickled is not None
-        return unpickled
 
 ############## EVERYTHING BEFORE THIS IS BUT HELPER FUNCTIONS
 ############## THESE BE THE TRU HEROS BEFORE YE
+
+
+    
 
 def find_similar_exhibits(museum_id, num_results):
     '''
@@ -134,19 +127,44 @@ def find_similar_exhibits(museum_id, num_results):
     difference = comparison[museum_id].sort_values()
     return difference.index[1,1+num_results]
     
-def key_word_search(key_words,num_results):
+def key_word_search(key_words,dim,exhibits):
     '''
     takes a dict {key word: number of times keyword occured}
     returns list of num_results most similar exhibits
     WARNING - I think we suffer from floating point error as some queeries
             return lots of 1s
     '''
-    dim, exhibits = ( get_files(s) for s in ('dim','exhibits') )
+    dim |= set(key_words.keys())
+    
     differences = [( x, cosine_distance(key_words,exhibits[x],dim) )
                     for x in exhibits]
     differences.sort(key=lambda x: x[1])
-    return differences[:num_results]
- 
+    return [d for d in differences if d[1]!=1] #filter out orthogonal seraches
+    
+    
+def search(args):
+    '''
+    Assuming we can get args_from_ui as a dict { 'field', ui input }
+    we search things and return results
+    '''
+    (dim,exhibits)=args['museums']
+    key_words = serachify[args['search_terms']]
+    search_results = key_word_search(key_words,dim,exhibits)
+    
 
 
-
+wordre = "[A-Za-z0-9'-]"
+def searchify(search_string):
+    '''
+    inputs: string
+    output: dictionary {word: count}
+    removes plurals 
+    '''
+    
+    l = re.findall()
+    
+    
+    ### turn numbers into words
+    ### remove capitals
+    
+default_search = {'yellow':4,'brick':5,'road':2,'I':1,'love':4,'ART':2}

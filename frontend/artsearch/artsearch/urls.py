1f5653aa847df6823dc5f7e859ffe8378c2762b2
caspern1 <casperneo@uchicago.edu>
2016-03-01 00:12:24 +0000

    added staticfiles_dirs to settings. removed +static settings from urls

--- frontend/artsearch/artsearch/urls.py
+++ frontend/artsearch/artsearch/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', 'finder.views.start'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]

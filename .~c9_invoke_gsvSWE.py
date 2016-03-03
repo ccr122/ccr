1760994c21eb0705343f5e0a8868e1203dffd5f4
caspern1 <casperneo@uchicago.edu>
2016-03-03 00:35:28 +0000

    redoing search as class

---
+++ .~c9_invoke_gsvSWE.py
@@ -0,0 +1,79 @@
+from num2Words import num2Words
+import re 
+
+wordre = "[A-Za-z0-9'-]+"
+
+INDEX_IGNORE = set(['a', 'about', 'affect', 'affects', 'all', 'among',
+                    'approximately' 'also', 'after', 'an',  'and',  
+                    'are', 'art', 'artist', 'artists', "artist's", 'arts', 'as',  'at', 
+                    'be', 'been', 'between', 'both', 'bring',
+                    'but',  'by', 'collect', 'collected', 'collection', 
+                    'contribute', 'contributed', 'come',
+                    'composition', 'display', 'displays', 
+                    'early', 'effect', 'effects', 
+                    'evoke', 'evoked', 'exhibit',
+                    'exhibits', 'exhibition', 'exhibitions',
+                    'experience', 'feature', 'features',
+                    'first', 'for',  'from', 'funding', 'go', 'goes', 
+                    'gone', 'had', 'have', 
+                    'has', 'he', 'her', 'hers', 
+                    'him', 'his',
+                    'how', 'i', 'ii', 'iii', 'idea', 'in',  
+                    'include', 
+                    'institute', 'interpret', 'interpretation', 'into',
+                    'is', 'it', 'just', 'many', 'meaning', 'more', 'motif', 
+                    'museum', 'not', 'of',
+                    'on', 'only',  'or', 'other', 'over', 
+                    'period', 'piece', 'pieces', 
+                    'presentation', 
+                    'second', 'see', 'significance', 'significant', 
+                    'so', 'society', 'span', 
+                    'special', 
+                    'sponsor', 
+                    'sponsors', 'sponsored', 'study',
+                    'such', 'support', 'than', 
+                    'that',  'the',  'their', 'third',
+                    'this', 'the', 'their', 'they', 'through',  'to', 'trust',
+                    'was', 'we', 'well', 'were', 'when', 'where', 
+                    'which', 'who', 'will', 'with', 'would', 
+                    'work', 'works', 'world', 'view',
+                    'until', 'visit', 'yet'])
+
+def str_to_dict(s):
+    '''
+    input: string
+    output: dictionary {word: count}
+    '''
+    word_dict = {}
+    l = re.findall(wordre, s)
+    for i in range(len(l)):
+        if l[i] can be int:             # turn numbers into words
+            l[i] = num2words(l[i])
+        
+        l[i] = l[i].lower()             # make all letters lowercase 
+        
+        if l[i][0] == "'":              # remove single quotes from beginning/
+            l[i] = l[i][1:]             # end of words in l
+        elif l[i][-1] == "'":
+            l[i] = l[i][:-1]
+            
+        if l[i] not in INDEX_IGNORE:
+            if l[i] not in word_dict:   # build dictionary
+                word_dict[l[i]] = 1
+            else:
+                word_dict[l[i]] += 1
+            
+    words = word_dict.keys()            # remove plurals
+    for i in range(len(words)):
+        if words[i][-1] == 's':
+            if words[i][:-1] == words[i-1]:
+                word_dict[i-1] += word_dict[i]
+                del word_dict[i]
+        elif words[i][-1] == 'es':
+            if words[i][:-2] == words[i-1]:
+                word_dict[i-1] += word_dict[i]
+                del word_dict[i]
+    
+    return word_dict
+        
+    
--- search/search.py
+++ search/search.py
@@ -9,6 +9,8 @@ import csv
 import pickle
 import os.path
 import re
+import numpy as np
+from num2words import num2words
 
 '''
 exhibits = pandas dataframe filtered down by museums
@@ -20,27 +22,124 @@ Also if we have time we should be able to update these data
     without reconstructing everything
 
 '''
+
+class Search():
+    '''
+    Contains things needed to search
+    '''
+    
+    def __init__(self,exhibits_file):
+        '''
+        Makes lots of commonly cited calculations for later use
+        '''
+        exhibits = pd.read_csv(exhibits_file,dtype={'ex_id':str})
+        self.words = list(exhibits['word'].unique())
+        self.ex_list = exhibits['ex_id'].unique()
+        self.num_ex  = len(self.ex_list)
+        self.num_ex_with_word = {}
+        for w in self.words:
+            self.num_ex_with_word[w] = len(exhibits[exhibits['word']==w]['ex_id'].unique())
+            
+        self.ex_vects = self.vectorize_exhibits(exhibits)
+        self.comparison_table = build_comparison_table()
+    
+    def tf_idf(self,term,document):
+        '''
+        term frequency-inverse document frequency (tf-idf)
+        this normalizes a given word so rarer words are worth more
+        '''
+        term_frequency = document[term]             
+        max_frequency_in_doc = max(document.values())    
+        tf = .5 +.5*(term_frequency/max_frequency_in_doc)             
+        num_rel_docs = 1 + self.num_ex_with_word.get(term,0)
+        idf = np.log( self.num_ex/num_rel_docs ) 
+        return tf*idf
+        
+    def vectorize_exhibits(self,exhibits):
+        '''
+        Makes tf_idf adjusted vector for each exhibit
+        data stored in { museum: { ex_id : vector}}
+        '''
+        return {}
+        
+    #Not done
+        
+    def build_comparison_table(self,exhibits):
+        '''
+        Makes a pandas data frame comparing exhibits
+        '''
+        comparison = pd.dataframe(columns = self.ex_list, index = self.ex_list)
+        
+        for ex in self.ex_list():
+            comparison[ex][ex] = 0.0
+        for (ex1,ex2) in itertools.combinations(self.ex_list,2):
+            v1 = self.ex_vects[ex1[:2]][ex1]
+            v2 = self.ex_vects[ex2[:2]][ex1]
+            d = spatial.distance.cosine(v1,v2)
+            comparison[ex1][ex2] = d
+            comparison[ex2][ex1] = d
+        
+        return comparison
+    
+    def compare_exhibits(self,ex_id,museums):
+        '''
+        return exhibits from given museums
+        sorted by cosine similarity
+        '''
+        return []
+        
+    #Not done
+    
+    def vectorize_dict(self,key_words):
+        '''
+        takes a dictionary of key words, turns it into a tfidf adjusted vector
+        '''
+        v = []
+        for w in self.words:
+            x = key_words.get(w,0)
+            assert type(x) == int
+            v.append(self.tf_idf(x,key_words))
+        return v
+        
+    def search(self,key_words,museums, num_results):
+        '''
+        Inputs:
+            key_words from search queery - {word:word count}
+            museums -   [museums to search from]
+            num_results
+        outputs:
+            closest num_result museums from your museums of choice
+        '''
+        search_vect = self.vectorize_dict(key_words)
+        results = []
+        for m in museums:
+            for ex in self.ex_vects[m]:
+                dist = spatial.distance.cosine(search_vect,self.ex_vects[m][ex])
+                results.append( (ex, dist))
+        results.sort(key=lambda x: x[1])
+        
+        return results[ : min(len(results), num_results)]
+        
+
 ##### Data related: getting and saving files, generating pandas tables
 
-FILE_PATHS = {  'dim'         : 'pickled_dim',
-#                'exhibits'    : 'pickled_exhibits',
-                'comparison'  : 'comparison_table',
-                'ex_desc_csv' : '../csvs/ex_id_to_ex_desc_parsed.csv'
+FILE_PATHS = {  
+ #               'dim'           : 'pickled_dim',
+#                'exhibits'      : 'pickled_exhibits',
+                'comparison'    : 'comparison_table',
+                'ex_desc_csv'   : '../csvs/ex_id_to_ex_desc_parsed.csv',
+                'ex_dict'       : 'pickled_ex_dict',
+                'search_object' : 'pickled_search_object'
                 }
-
-def make_dim_exhibits(museums):
+"""
+def make_dim_exhibits():
     '''
     takes a list of museum ids: museums = ['001','002',...]
     makes dim - set of all words that appeared in dataframe
     makes filtered - dataframe filtered by museum
     '''
-    exhibits = pd.read_csv( FILE_PATHS['ex_desc_csv'] )
-    if museums is not None:
-        filter_ = [str(x) for x in exhibits['ex_id']]
-        museums = [m.lstrip('0') for m in museums]
-        filter_ = [ x[:-2] in museums for x in filter_]
-        exhibits = exhibits[filter_]
-    dim = set(exhibits.groupby('word').groups.keys())
+    exhibits = pd.read_csv( FILE_PATHS['ex_desc_csv'] ,dtype={'ex_id':str})
+    dim = exhibits['word'].unique()
     return (dim, exhibits)
 
 def make_ex_dict(exhibits):
@@ -50,7 +149,7 @@ def make_ex_dict(exhibits):
         words = [exhibits.loc[ row,'word'] for row in rows]
         words_dict = {}
         for w in words:
-            if words_dict.get(w)==None:
+            if words_dict.get(w) == None:
                 words_dict[w] = 1
             else:
                 words_dict[w] += 1
@@ -62,71 +161,122 @@ def build_comparison_table():
     Makes table of cosine_distance between descriptions
     index and columns are all exhibit ids
     '''
-    (dim,exhibits) = make_dim_exhibits(None)
+    (dim,exhibits) = make_dim_exhibits()
     ex_dict = make_ex_dict(exhibits)
     comparison = pd.DataFrame(  columns = ex_dict.keys(),
                             index = ex_dict.keys())
-    for x in ex_dict:
+    for ex in ex_dict:
         comparison[ex][ex]=0.0
     for (ex1,ex2) in itertools.combinations(ex_dict,2):
-        d = cosine_distance(ex_dict[ex1] ,ex_dict[ex2],dim)
+        d = cosine_distance(ex_dict[ex1] ,ex_dict[ex2],dim,exhibits)
         comparison[ex1][ex2] = d
         comparison[ex2][ex1] = d
     
-    comparison = comparision.rename(columns={c.columns[0]:'ex_id'})
-    comparison.to_csv(FILE_PATHS['comparison'])
-    
-def update_comparison_table(exhibits,comparison):
-    '''
-    In case we decide to get more museums we should update comparison
-    rather than build it from scratch
-    '''
-    pass
+    comparison = comparision.rename(columns={comparison.columns[0]:'ex_id'})
+    return comparison
 
 def get_comparison_table():
+    '''
+    Checks if it is saved, builds if its not
+    '''
     c = FILE_PATHS['comparison']
     if os.path.isfile(c):
         return pd.read_csv(c)
     else:
-        build_comparison_table() #makes and saves table
+        comparison = build_comparison_table()
+        comparison.to_csv(FILE_PATHS['comparison'])
     assert os.path.isfile(c)
     return pd.read_csv(c)
+    
+def get_ex_dict():
+    '''
+    checks if it is saved, builds if it is not
+    '''
+    fp = FILE_PATHS['ex_dict']
+    
+    if os.path.isfile(fp):
+        with open(fp,'r') as pik:
+            ex_dict = pickle.load(pik)
+    else:
+        (d,e) = make_dim_exhibits()
+        ex_dict = make_ex_dict(e)
+        with open(fp,'w') as pik:
+            pickle.dump(ex_dict,pik)
+    return ex_dict
 
+def reset_files():
+    '''
+    For overwriting purposes
+    '''
+    for path in ['comprison','ex_dict']:
+        os.remove( FILE_PATHS[path] )
+    
+        
 ######### Cosine similarity
 
-def make_vector(words,dim):
+def tfidf(t,d,D):
+    '''
+    term frequency-inverse document frequency
+    this normalizes the corpus so rarer words are worth more
+        t = term
+        d = document
+        D = { documents in corpus}
+    '''
+    return tf(t,d)*idf(t,D)
+    
+def tf(t,d):
+    '''
+    term_frequency = frequency(term,document)
+    mnt = max{frequency(term',document) : term' in document }
+    '''
+    ft = d[t] 
+    mnt = max(d.values())
+    return .5 + .5*(ft/mnt)
+
+def idf(t,D):
+    '''
+    N is number of documents
+    Ndf is number of documents with term t
+    '''
+    N = len(D['ex_id'].unique())
+    Ndf = len(D[D['word']==t]['ex_id'].unique())
+    return np.log( N/Ndf )
+
+def make_vector(words,dim,exhibits):
     '''
     Changes a dict of {word:word count}
     into list (vector)
+    each entry is normalized by tfidf
     '''
     vect = []
-    for d in dim:
-        x = words.get(d)
-        if x == None:
-            vect+=[0]
-        else:
+    for t in dim:
+        x = words.get(t)
+        if x is not None:
             assert type(x)==int
-            vect+=[x]
+            vect+=[tfidf(t,words,exhibits)]
+        else:
+            vect+=[0]
     return vect
 
-def cosine_distance(words1,words2,dim):
+def cosine_distance(words1,words2,dim,exhibits):
     '''
     Takes 2 dictionary of {word:word count} and list [all words]
     returns cosine similarity between words1,words2
     '''
-    a = make_vector(words1,dim)
-    b = make_vector(words2,dim)
+    a = make_vector(words1,dim,exhibits)
+    b = make_vector(words2,dim,exhibits)
     return spatial.distance.cosine(a,b)
 
 ###### Search tools
 
-def find_similar_exhibits(museum_id, num_results):
+def find_similar_exhibits(e_id, num_results):
     '''
     Given an exhibit, returns the num_results most similar exhibitions
     trivially the most similar exhibition is itself so we skip that one.
     '''
-    difference = comparison[museum_id].sort_values()
-    return difference.index[1,1+num_results]
+    comparison = get_comparison_table()
+    difference = comparison[e_id].sort_values()
+    return difference.index[1,min(1+num_results,len(difference))]
     
 def key_word_search(key_words,dim,exhibits):
     '''
@@ -135,28 +285,16 @@ def key_word_search(key_words,dim,exhibits):
     WARNING - I think we suffer from floating point error as some queeries
             return lots of 1s
     '''
-    ex_dict = make_ex_dict(exhibits)
-    dim |= set(key_words.keys())
-    differences = [( x, cosine_distance(key_words,ex_dict[x],dim) )
+    ex_dict = get_ex_dict()
+    search_vect = make_vector(key_words,dim,exhibits)
+    differences = [( x, spatial.distance.cosine(search_vect,make_vector(ex_dict[x],dim,exhibits)))
                     for x in ex_dict]
     differences.sort(key=lambda x: x[1])
-    return [d[0] for d in differences if d[1]!=1] #filter out orthogonal seraches
+    return differences
+    #return [d[0] for d in differences if d[1]!=1] #filter out orthogonal seraches
+"""
 
-def searchify(search_string):
-    '''
-    inputs: string
-    output: dictionary {word: count}
-    removes plurals 
-    '''
-    wordre = "[A-Za-z0-9'-]"
-    l = re.findall()
-    
-    ### turn numbers into words
-    ### remove capitals
-    ### also apply this function to parsing scraped data
-    
 
-    
 ##### Search main
 '''
 Assuming args_from_ui is something like
@@ -176,13 +314,38 @@ search should return:
     ranked by search similarity or by date or something
 '''
 
+def get_search_object(force = False):
+    '''
+    checks if it is saved, builds if it is not
+    '''
+    fp = FILE_PATHS['search_object']
+    
+    if os.path.isfile(fp) or not force:
+        with open(fp,'r') as pik:
+            S = pickle.load(pik)
+    else:
+        with open(fp,'w') as pik:
+            S = Search( FILE_PATHS['ex_desc_csv'] )
+            pickle.dump( S ,pik)
+    return S
+
+
+def searchify(search_text):
+    '''
+    takes search term string
+    returns dictionary {normalized_word : count}
+    '''
+    return{}
+
 def search(args):
     '''
     Assuming we can get args_from_ui as a dict { 'field', ui input }
     we search things and return results
     '''
-    (dim,exhibits)=args.get('museums')
-    key_words = serachify[args['search_terms']]
-    search_results = key_word_search(key_words,dim,exhibits)
+    key_words = searchify( args['seach_field'] )
+    museums = args['museums']
+    num_results = args['num_results']
+    s = get_search_object()
+    s.search(key_words,museums,num_results)
     
 default_key_word_search = {'yellow':4,'brick':5,'road':2,'I':1,'love':4,'ART':2,'SqUaRE':1}
